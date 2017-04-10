ad4e672bc35b36d32bccae00b9f16a4d4b2ca812
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-04-10 01:37:17 -0700

    Compiled

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,12 @@ $(function() {
 	$('.about-btn').click(function() {
 		$('html, body').animate({
 			scrollTop: $('#about').offset().top
-		}, 1000, "swing");
+		}, 1000, 'swing');
 	});
 	$('.intro-btn').click(function() {
 		$('html, body').animate({
 			scrollTop: 0
-		}, 1000, "swing");
+		}, 1000, 'swing');
 	});
 
 	$('#intro h1').addClass('animated fadeInUp');
