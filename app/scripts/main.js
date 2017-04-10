1f271a483458dcc12710c09dc6cd2c8fa91e2674
PeterTheDestroyer <peter.lee@berkeley.edu>
2017-04-10 01:23:39 -0700

    Added basics

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,14 @@
 $(function() {
 	$('.parallax').parallax();
+
+	$('.about-btn').click(function() {
+		$('html, body').animate({
+			scrollTop: $('#about').offset().top
+		}, 1000, "swing");
+	});
+	$('.intro-btn').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000, "swing");
+	});
 });
